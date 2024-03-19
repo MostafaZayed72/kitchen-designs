e092f7864be6779e8b1175d79a775c7879b744d4
MostafaZayed72 <143009621+MostafaZayed72@users.noreply.github.com>
2024-03-19 13:39:37 +0200

    Add files via upload

---
+++ layouts/dark-green.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dark-green">
+    <CommonMouseCursor />
+    <CommonScrollToTop />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+onMounted(() => {
+  if (typeof WOW !== 'undefined') new WOW().init();
+  if (typeof Splitting !== 'undefined') Splitting({ target: "[data-splitting]" });
+});
+
+useHead({
+  link: [
+    { rel: "stylesheet", href: "/assets/css/style.css" },
+    { rel: "stylesheet", href: "/assets/css/skin-green.css" }
+  ],
+});
+</script>
---
+++ layouts/dark.vue
@@ -0,0 +1,18 @@
+<template>
+  <CommonMouseCursor />
+  <CommonScrollToTop />
+  <slot />
+</template>
+
+<script setup>
+onMounted(() => {
+  if (typeof WOW !== 'undefined') new WOW().init();
+  if (typeof Splitting !== 'undefined') Splitting({ target: "[data-splitting]" });
+});
+
+useHead({
+  link: [
+    { rel: "stylesheet", href: "/assets/css/style.css" }
+  ],
+});
+</script>
